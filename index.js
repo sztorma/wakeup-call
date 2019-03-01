69c34102af56de75ab3a0931e05c4e1e34bf1ea5
Sztorma <sztorma@outlook.com>
2019-03-01 10:55:36 +0100

    dinamic port added to index.js

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var app = express();
 
-app.listen(3000, function(){
-    console.log("Listening on port 3000!")
-  });
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
 
 var http = require("http");
 setInterval(function() {
